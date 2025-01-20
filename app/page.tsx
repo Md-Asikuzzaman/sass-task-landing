4e66ff966026ecb5c76b9783b9ce73e74601132c
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-01-20 12:16:31 +0600

    feat: setup files and folders structures

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <div>
-      <h1>home</h1>
+      <h1>Hello World!!! My name is asik, what is your name as far something for all demos and takes </h1>
     </div>
   );
 }
