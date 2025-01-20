d3de351a07a4c4305328ebe457bfbcedc4fbe1e6
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-01-20 21:03:11 +0600

    feat: added logo-ticker section

---
+++ app/sections/LogoTicker.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Container from '../components/Container';
+import Image from 'next/image';
+import { logos } from '../utils/data';
+
+const LogoTicker = () => {
+  return (
+    <section className='bg-black text-white py-[72px] sm:py-24'>
+      <Container>
+        <h2 className='text-xl text-center text-white/70'>
+          Trusted by the world's most innovative teams.
+        </h2>
+        <div className='relative overflow-hidden mt-9 mask-overlay'>
+          <div className='flex gap-16'>
+            {[...logos, ...logos].map(({ src, alt }, i) => (
+              <Image
+                key={i}
+                src={src}
+                alt={alt}
+                height={140}
+                width={140}
+                className='h-8 w-auto flex-none'
+              />
+            ))}
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default LogoTicker;
---
+++ app/utils/data.ts
@@ -0,0 +1,8 @@
+export const logos = [
+  { src: '/images/logo/acme.png', alt: 'Acme Logo' },
+  { src: '/images/logo/quantum.png', alt: 'Quantum Logo' },
+  { src: '/images/logo/echo.png', alt: 'Echo Logo' },
+  { src: '/images/logo/celestial.png', alt: 'Celestial Logo' },
+  { src: '/images/logo/pulse.png', alt: 'Pulse Logo' },
+  { src: '/images/logo/apex.png', alt: 'Apex Logo' },
+];
