9fd35ddb68f227f87c4e4660d87ec1e24622e4db
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-01-22 13:08:19 +0600

    fix: added UI image in call-to-action section

--- app/sections/CallToAction.tsx
+++ app/sections/CallToAction.tsx
@@ -5,11 +5,12 @@ import Container from '../components/Container';
 import { SectionTitle } from '../components/SectionTitle';
 import SectionDesc from '../components/SectionDesc';
 import { Button } from '../components/Button';
+import Image from 'next/image';
 
 const CallToAction = () => {
   return (
     <section className='bg-black text-white py-[72px] md:py-24'>
-      <Container>
+      <Container className='relative max-w-xl'>
         <SectionTitle title='Get instant access' />
         <SectionDesc
           description='Celebrate the joy accomplishment with an app designed to track your progress and motivate your efforts.'
@@ -25,6 +26,14 @@ const CallToAction = () => {
             Get access
           </Button>
         </form>
+        {/* ui design */}
+
+        <div className='h-48 w-48 absolute -top-[50px] -left-[200px]'>
+          <Image src={'/images/ui/emojistar.png'} alt={'img'} fill />
+        </div>
+        <div className='h-48 w-48 absolute top-[10px] -right-[200px]'>
+          <Image src={'/images/ui/helix2.png'} alt={'img'} fill />
+        </div>
       </Container>
     </section>
   );
