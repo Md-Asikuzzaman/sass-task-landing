52c4e15149a694107682e463f3e53cf7e9998e26
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-01-22 14:41:56 +0600

    fix: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { DM_Sans } from 'next/font/google';
 import clsx from 'clsx';
 import './globals.css';
@@ -8,33 +8,32 @@ const dmSans = DM_Sans({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Sass Time Landing Page',
   description: 'One Task at a Time',
-  icons: {
-    icon: '/images/logo/logo.png',
-    shortcut: '/images/logo/logo.png',
-    apple: '/images/logo/logo.png',
-  },
   openGraph: {
     title: 'Sass Time Landing Page',
     description: 'One Task at a Time',
-    url: 'https://sass-time-landing.com',
+    url: 'https://sass-time-landing.vercel.app',
     siteName: 'Sass Time',
     images: [
       {
-        url: '/images/logo/logo.png',
-        width: 800,
-        height: 600,
+        url: 'https://sass-time-landing.vercel.app/images/logo/logo.png', // Absolute URL for OG image
+        width: 1200, // Recommended size for OG
+        height: 630,
         alt: 'Sass Time Logo',
       },
     ],
+    locale: 'en_US',
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Sass Time Landing Page',
     description: 'One Task at a Time',
-    images: ['/images/logo/logo.png'],
+    images: ['https://sass-time-landing.vercel.app/images/logo/logo.png'], // Absolute URL for Twitter image
   },
-  themeColor: '#ffffff',
+};
+
+export const viewport: Viewport = {
+  themeColor: 'black',
 };
 
 export default function RootLayout({
@@ -44,6 +43,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/images/logo/logo.png' sizes='any' />
+      </head>
       <body className={clsx(dmSans.className, 'antialiased')}>{children}</body>
     </html>
   );
