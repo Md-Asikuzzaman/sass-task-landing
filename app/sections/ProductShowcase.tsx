d437dbbc917ea6213bcbf9a6b5bb4f1d88362e69
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2025-01-27 21:23:02 +0600

    feat: added animation on product-showcase section

--- app/sections/ProductShowcase.tsx
+++ app/sections/ProductShowcase.tsx
@@ -1,12 +1,24 @@
 'use client';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import Image from 'next/image';
 import Container from '../components/Container';
 import { SectionTitle } from '../components/SectionTitle';
 import SectionDesc from '../components/SectionDesc';
 
+import { motion, useScroll, useTransform } from 'framer-motion';
+
 const ProductShowcase = () => {
+  const appScreenRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: appScreenRef,
+    offset: ['start end', 'end end'],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+
   return (
     <section className='bg-black text-white bg-gradient-to-b from-black to-[#5D2CAB] py-[72px] md:py-24'>
       <Container>
@@ -17,7 +29,15 @@ const ProductShowcase = () => {
           progress, motivate your efforts, and celebrate your success, one task
           at a time.'
         />
-        <div className='flex items-center justify-center'>
+        <motion.div
+          className='flex items-center justify-center'
+          style={{
+            opacity,
+            transformPerspective: '800px',
+            rotateX,
+          }}
+          ref={appScreenRef}
+        >
           <Image
             className='mt-14'
             src={'/images/logo/app-screen.png'}
@@ -27,7 +47,7 @@ const ProductShowcase = () => {
             sizes='(max-width: 768px) 100vw, (max-width: 1400px) 100vw, 100vw'
             style={{ width: 'auto', height: 'auto' }}
           />
-        </div>
+        </motion.div>
       </Container>
     </section>
   );
